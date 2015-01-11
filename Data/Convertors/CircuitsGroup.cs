163353313f47b3c196854faf344b0ff7ac3e9d8b
Stanilav Hacker <info@dbsys.cz>
2015-01-11 13:57:23 +0100

    Přesun CircuitsGroup.cs na statistiky

--- Data/Convertors/CircuitsGroup.cs
+++ Data/Convertors/CircuitsGroup.cs
@@ -10,70 +10,12 @@ using System.Windows.Data;
 
 namespace CoPilot.Speedway.Data.Convertors
 {
-    public class CircuitGroup
-    {
-        public List<State> States { get; set; }
-        public List<Double> Laps { get; set; }
-        public Circuit Circuit { get; set; }
-        public String Name { get; set; }
-
-        public TimeSpan FastestLap
-        {
-            get
-            {
-                TimeSpan best = TimeSpan.MaxValue;
-                foreach (var lap in Laps)
-                {
-                    TimeSpan lapTime = new TimeSpan(0, 0, 0, 0, (int)lap);
-                    if (lapTime < best)
-                    {
-                        best = lapTime;
-                    }
-                }
-                return best;
-            }
-        }
-    }
-
     public class CircuitsGroup : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             ObservableCollection<Circuit> circuits = (ObservableCollection<Circuit>)value;
-            Dictionary<string, CircuitGroup> groups = new Dictionary<string, CircuitGroup>();
-
-            for (var i = 0; i < circuits.Count; i++)
-            {
-                CircuitGroup group = null;
-                var circuit = circuits[i];
-                var exists = groups.ContainsKey(circuit.Id);
-                //no exists
-                if (!exists)
-                {
-                    group = new CircuitGroup();
-                    //create
-                    group.Laps = new List<double>();
-                    group.States = new List<State>();
-                    //fill
-                    group.Name = circuit.Name;
-                    group.States.AddRange(circuit.States);
-                    group.Circuit = circuit;
-                    //add
-                    groups.Add(circuit.Id, group);
-                }
-                //get
-                group = groups[circuit.Id];
-                //find fastest circle
-                if (group.Circuit.Laps.Min() > circuit.Laps.Min())
-                {
-                    group.Circuit = circuit;
-                }
-                //add laps
-                group.Laps.AddRange(circuit.Laps);
-            }
-
-            //return
-            return groups.Values.ToList();
+            return CoPilot.Statistics.Statistics.GroupedCircuits(circuits);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
--- Speedway/View/SpeedWay.xaml.cs
+++ Speedway/View/SpeedWay.xaml.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
 using PR = System.Windows.Controls.Primitives;
 using Controllers = CoPilot.Speedway.Controller;
 using System.ComponentModel;
@@ -38,8 +34,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Media.PhoneExtensions;
-using CoPilot.Speedway.Data.Convertors;
 using System.Text;
+using CoPilot.Statistics.Utils;
 
 namespace CoPilot.Speedway.View
 {
